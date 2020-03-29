ae4f7d2ce0a8551a0757e6d7c1874c10d572ad60
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-29 23:09:16 +0700

    Membuat ResponsModel

--- app/src/main/java/com/takatutustudio/appbiodata/api/Retroserver.java
+++ app/src/main/java/com/takatutustudio/appbiodata/api/Retroserver.java
@@ -1,4 +1,20 @@
 package com.takatutustudio.appbiodata.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Retroserver {
+    private static final String base_url = "http://192.168.43.4/api_crud/insert.php";
+
+    private static Retrofit retrofit;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(base_url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
---
+++ app/src/main/java/com/takatutustudio/appbiodata/model/ResponsModel.java
@@ -0,0 +1,4 @@
+package com.takatutustudio.appbiodata.model;
+
+public class ResponsModel {
+}
